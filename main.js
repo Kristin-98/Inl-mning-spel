240d888c74d4c8524f8886db0187662a8ca3cfbf
Kristin-98 <kristi98_s@hotmail.se>
2024-11-10 18:14:11 +0100

    function1

--- main.js
+++ main.js
@@ -1,22 +1,49 @@
 //Kör main funtionen när domen är laddad.
 window.addEventListener("DOMContentLoaded", main);
 
-function main(){
-    console.log();
-
-}
-
-function startGame() {
-    document.querySelector(".start").style.display = "none";
-    document.getElementById("gameScreen").style.display = "block";
-    
-}
-
-function plantFlowers() {
-    console.log();
+function main() {
+    loadStartScene();
 }
 
 function loadStartScene() {
+    container.innerHTML = "";
+
     const text = document.createElement("p");
-    text.textContent
+    text.textContent = "startDescription";
+    text.className = "start";
+
+    const button = document.createElement("button");
+    button.textContent = "Börja Spela";
+    button.onclick = loadSecondScene;
+
+    const image1 = document.createElement("img");
+    image1.src = "Skärmbild (127).png";
+    image1.alt = "Garden";
+    image1.className = "start-image";
+
+    container.append(image1, text, button);
 }
+
+
+
+
+
+
+
+
+
+// function startGame() {
+//     document.querySelector(".start").style.display = "none";
+//     document.getElementById("gameScreen").style.display = "block";
+    
+// }
+
+// function plantFlowers() {
+//     console.log();
+// }
+
+// function loadStartScene() {
+//     const text = document.createElement("p");
+//     text.textContent
+// }
+
